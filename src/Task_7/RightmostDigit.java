2a2b79e206bb0c31e5d82374380890f9cf117a96
NoelVecug <geesegagaga@mail.ru>
2023-01-26 17:31:47 +0300

    commit

--- src/Task_7/RightmostDigit.java
+++ src/Task_7/RightmostDigit.java
@@ -1,33 +1,40 @@
 package Task_7;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class RightmostDigit {
-    private static final int CAPACITY = 3;
-    private static ArrayList<Integer> nums = new ArrayList<>(CAPACITY);
     public static void main(String[] args) {
         // Write a Java program that accepts three integers from the user
         // and return true if two or more of them (integers ) have the same rightmost digit.
         // The integers are non-negative.
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Input tree positive digits through 'Enter'.");
-        for (int i = 0; i < CAPACITY; i++) {
-            int num = scanner.nextInt();
-            CheckPositive(num);
-        }
-        for (int c: nums) {
-            System.out.println(nums.get(c));
-        }
+        System.out.println("Input three positive digits.");
 
+        System.out.print("First: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Second: ");
+        int num2 = scanner.nextInt();
+        System.out.print("Third: ");
+        int num3 = scanner.nextInt();
 
-    }
-    public static void CheckPositive(int num) {
-        if (num < 0) {
-            System.out.println("Negative digit. Please try again.");
+        if (num1 < 0 || num2 < 0 || num3 < 0) {
+            System.out.println("Error: Find a negative digit.");
         } else {
-            nums.add(num);
+            int max = Integer.max(num1, num2);
+            int greatestDigit = Integer.max(max, num3);
+            while (greatestDigit > 0) {
+                if (num1 % greatestDigit == 0 && num2 % greatestDigit == 0) {
+                    System.out.println("Greatest division: " + greatestDigit);
+                    break;
+                } else if (num1 % greatestDigit == 0 && num3 % greatestDigit == 0) {
+                    System.out.println("Greatest division: " + greatestDigit);
+                    break;
+                } else if (num2 % greatestDigit == 0 && num3 % greatestDigit == 0) {
+                    System.out.println("Greatest division: " + greatestDigit);
+                    break;
+                } else greatestDigit--;
+            }
         }
     }
 }
