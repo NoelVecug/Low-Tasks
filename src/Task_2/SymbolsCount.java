1f1a47ebdd3b37a3abf40db8a58e56d0c72c6d28
iiandmy <frezhgrand@gmail.com>
2023-01-29 22:12:32 +0300

    review task 2

--- src/Task_2/SymbolsCount.java
+++ src/Task_2/SymbolsCount.java
@@ -1,42 +1,51 @@
 package Task_2;
 
-import java.util.Scanner;
+// Write a Java program to count the letters,
+// spaces, numbers and other characters of an input string.
+
+/*
+    Тут в целом с вынесением метода та же ошибка.
+    И еще на заметку не ебись со сканером в таких заданиях, это бесполезно
+    Тем более ты его не закрыл, что чревато утечками в больших проектах.
+    Сканнер это ресурс который надо закрывать.
+    По поводу твоего метода его можно было и не искать.
+    У тебя проверки на все были, в каждой из них добавил оператор continue,
+    Что позволит избежать бессмысленных проверок и как раз посчитать твой otherCount, потому
+    Что по сути все, что не нашло до этого входит в otherCount.
+ */
 
 public class SymbolsCount {
     public static void main(String[] args) {
-        // Write a Java program to count the letters,
-        // spaces, numbers and other characters of an input string.
+        String input = "ABC 123 123 ]]1%%$#";
+        calcStringSymbols(input);
+    }
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Input a string with random symbols");
-        char[] str = scanner.nextLine().toCharArray();
-        StringBuilder fullStr = new StringBuilder();
+    private static void calcStringSymbols(String input) {
         int alphabetCount = 0;
         int spaceCount = 0;
         int numbersCount = 0;
         int otherCount = 0;
-        for (char c : str) {
-            fullStr.append(c);
+
+        for (char c : input.toCharArray()) {
             if (Character.isAlphabetic(c)) {
                 alphabetCount++;
+                continue;
             }
             if (Character.isWhitespace(c)) {
                 spaceCount++;
+                continue;
             }
             if (Character.isDigit(c)) {
                 numbersCount++;
+                continue;
             }
-            if (Character.isBmpCodePoint(c) && (!Character.isDigit(c) && !Character.isWhitespace(c)
-            && !Character.isAlphabetic(c))) {
-                // I don't find Character method for "other" symbols
-                otherCount++;
-            }
+            otherCount++;
         }
         System.out.printf("""
                 The string: %s
                 letter: %d
                 space: %d
                 number: %d
-                other: %d""", fullStr, alphabetCount, spaceCount, numbersCount, otherCount);
+                other: %d""", input, alphabetCount, spaceCount, numbersCount, otherCount);
     }
 }
